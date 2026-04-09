44527ad9d10c37201f8349904ac13e5f9fba2d6b
BharathHU <bharathhubharath@gmail.com>
2026-04-09 14:35:32 +0530

    updated

---
+++ dictionary in python/dict.py
@@ -0,0 +1,9 @@
+emp={"id":101,"name":"Bharath","age":21,"addr":"Bengaluru"}
+print(emp)
+print(type(emp))
+print(emp["name"])
+print(emp["addr"])
+emp["mobile"]=7975414457
+print(emp)
+del emp["mobile"]
+print(emp)
