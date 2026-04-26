255e0c7801a7d0dc829ba90bca356cf0449ec379
BharathHU <bharathhubharath@gmail.com>
2026-04-26 20:18:30 +0530

    updated

--- Regular Expression/MetaCharecters.py
+++ Regular Expression/MetaCharecters.py
@@ -36,3 +36,93 @@ text="a Python is a ython pppython"
 regEx=r"is|super"
 result=re.findall(regEx,text,re.IGNORECASE)
 print(result)
+#==================================================================================
+import re 
+text="Python is a language"
+regEx=r"is| "
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+#===================================================================================
+import re 
+text="Python is a super"
+regEx=r"is|"
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+
+#====================================================================================
+# * metacharecter:
+# it is will check the occurence of charecter for 0 or more time (0 - ∞).
+print("* metacharecter:")
+import re 
+text="Python is a language pppython not ython and pppppppython"
+regEx=r"p*ython"
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+
+#======================================================================================
+# + metacharecter:
+# It will check the occurence of the charecter for one or more time (1 - ∞).
+print("+ metacharecter:")
+import re 
+text="Python is a language pppython not ython and pppppppython"
+regEx=r"p+ython"
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+#======================================================================================
+#  ? metacharecter:
+# It will check the occurence of the cherecter for 0 or 1 time(0 - 1).
+print("? metacharecter:")
+import re 
+text="Python is pppython not ython and pppppppython"
+regEx=r"p?ython"
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+#======================================================================================
+#  ^ metacharecter:
+# It is used to search the pattern at the beginning of the string.
+print("^ metacharecter:")
+import re 
+text="a Python is pppython not ython and pppppppython"
+regEx=r"^a"
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+#========================================================================================
+# $ metacharecter:
+# It is used to search the pattern at the end of the string.
+print("$ metacharacter:")
+import re 
+text="a Python is pppython not ython and pppppppython"
+regEx=r"pppppppython$"
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+#=========================================================================================
+# [] Square brackets metacharecter:
+# any charecter declare inside square bress it will search for the particular charcter in the string.
+print("[] square bracket metacharecter:")
+import re 
+text="a Python is pppython not ython and pppppppython"
+regEx=r"[aeiou]"
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+#=========================================================================================
+#{} curly brackets metacharecter:
+# It is used to specify the number of occurence of the charecter. by using {},or
+# we can declare the occurence of the charecter min and max time.
+print("{} square bracket metacharecter:")
+import re 
+text="a Python is pppython not ython and pppppppython"
+regEx=r"p{2,3}ython" # limite only applicable for p
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+#===========================================================================================
+import re 
+text="a Python is pppython not ython and pppppppython"
+regEx=r"p{,2}ython" # limite only applicable for p
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
+#===========================================================================================
+import re 
+text="a Python is pppython not ython and pppppppython"
+regEx=r"p{2}ython" # limite only applicable for p
+result=re.findall(regEx,text,re.IGNORECASE)
+print(result)
---
+++ Regular Expression/RegEx.py
@@ -0,0 +1,14 @@
+# String along with meta charecter is refer to as regular Expression or pattern matching is also refer to as regular expression.
+# mainly used in:
+# 1.Search Engine
+# 2.REtrive the data from large dataset.
+# 3. To validate email and Mobile numbers.
+# In regEx mainly we have 3 in build methods.
+# 1. match():
+#     it is used to search the maching pattern or substring only at the begining of the string.
+# 2. search():
+# it is used to search the maching pattern or substring of the pattern throughout the string,
+# but it will consider the first occuring substring or the pattern
+
+# 3. findall():
+# it is used to search the maching pattern or substring throghout the string and matching substring are written within the list.
